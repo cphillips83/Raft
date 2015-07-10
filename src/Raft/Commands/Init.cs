dc893ccc94cd05548a8673b12a3bafa52fe09276
Chris Phillips <chris.phillips@mindmatrix.net>
2015-07-10 16:41:13 -0400

    working on command parsing

---
+++ src/Raft/Commands/Init.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using ManyConsole;
+
+namespace Raft.Commands
+{
+
+    public abstract class Argument
+    {
+        public string Command, Description;
+        public bool Required = false;
+        public bool Supplied;
+        public bool Parsed;
+
+        public Argument(string command, string desc, bool required = false)
+        {
+            Command = command;
+            Description = desc;
+            Required = required;
+        }
+
+        public void Parse(string s)
+        {
+            Supplied = true;
+            Parsed = parse(s);
+        }
+
+        protected abstract bool parse(string s);
+
+        public void Register(ConsoleCommand cmd)
+        {
+            if (Required)
+                cmd.HasRequiredOption(Command, Description, Parse);
+            else
+                cmd.HasOption(Command, Description, Parse);
+        }
+    }
+
+
+
+    public class IPArgument : Argument
+    {
+        public IPAddress Value;
+        public IPArgument(string command, string desc, bool required = false)
+            : base(command, desc, required)
+        { }
+
+        protected override bool parse(string s)
+        {
+            return IPAddress.TryParse(s, out Value);
+        }
+    }
+
+    public abstract class IDCommand : ConsoleCommand
+    {
+        protected bool parseSuccess = true;
+
+        protected IPArgument _ip = new IPArgument("ip=", "IP address to listen on, also identifies the server (CAN'T CHANGE)", true);
+        protected int _port;
+        protected string _dataDir;
+
+        protected List<Argument> _commands = new List<Argument>();
+
+        protected IDCommand()
+        {
+            _commands.Add(_ip);
+
+            this.HasRequiredOption<int>("port=", "Port to listen on, forms unique ID combined with IP", s => _port = s);
+            this.HasRequiredOption("dataDir=", "Folder to store raft data", s => _dataDir = s);
+
+            buildCommands();
+
+            foreach (var cmd in _commands)
+                cmd.Register(this);
+
+        }
+
+        protected abstract void buildCommands();
+
+        public override int Run(string[] remainingArguments)
+        {
+            if (!_ip.Parsed)
+                Console.WriteLine("failed to parse ip");
+
+            if (_port < 1 || _port > 65536)
+            {
+                Console.WriteLine("port is expected to be between 1 and 65536");
+                return 1;
+            }
+
+            return Process(remainingArguments);
+        }
+
+        protected abstract int Process(string[] remainingArguments);
+
+        //protected void parseIP(string s)
+    }
+
+    public class Init : IDCommand
+    {
+        private int _initialSize, _maxSize;
+
+        protected override void buildCommands()
+        {
+            this.SkipsCommandSummaryBeforeRunning();
+
+            this.IsCommand("init", "Initializes a new cluster with this being the first server in the cluster");
+
+            this.HasOption<int>("initialSize", "initial size of the data file", s => _initialSize = s);
+            this.HasOption<int>("maxSize", "max storage size of the data file", s => _maxSize = s);
+        }
+
+        protected override int Process(string[] remainingArguments)
+        {
+
+            Console.WriteLine(_ip.Value);
+
+
+            return 0;
+        }
+    }
+}
--- src/Raft/Program.cs
+++ src/Raft/Program.cs
@@ -12,6 +12,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Lidgren.Network;
+using ManyConsole;
 using Raft.Logs;
 using Raft.Messages;
 using Raft.States;
@@ -19,36 +20,36 @@ using Raft.Transports;
 
 namespace Raft
 {
-    //Haystack tech talk http://www.downvids.net/haystack-tech-talk-4-28-2009--375887.html
-
-    /* Design Ideas
-     *  Modeling after haystack
-     *  
-     * Data Store
-     *  Each store will have several physical volumes
-     *  Each physical volume can be in multiple logical volumes
-     *  Logical volumes can span across data centers
-     *  
-     * 
-     */
-
-
-    public static class Helper
+    static class Program
     {
-        public static int id;
-
-        public static Server CreateServer()
+        public static void ForAll<T>(this IEnumerable<T> array, Action<T> action)
         {
-            var sid = ++id;
-            var port = sid + 7000;
-
-            return new Server(new IPEndPoint(IPAddress.Loopback, port));
+            foreach (var item in array)
+                action(item);
         }
 
-    }
+        static int Main(string[] args)
+        {
+            try
+            {
+                var commands = new List<ConsoleCommand>();
 
-    class Program
-    {
+                //need data directory
+                commands.Add(new Commands.Init());
+
+                
+
+                // then run them.
+                return ConsoleCommandDispatcher.DispatchCommand(commands as IEnumerable<ConsoleCommand>, args, Console.Out, false);
+            }
+            finally
+            {
+                Console.ResetColor();
+                Console.Read();
+            }
+        }
+
+
         static void DoTest(int count)
         {
             var transport = new MemoryTransport(10, 15, 0);
@@ -82,48 +83,48 @@ namespace Raft
 
         }
 
-        static void Main(string[] args)
-        {
-            DoTest(3);
-            Console.Read();
+        //static void Main(string[] args)
+        //{
+        //    DoTest(3);
+        //    Console.Read();
 
-            var s1 = new Server(new IPEndPoint(IPAddress.Loopback, 7741));
-            var s2 = new Server(new IPEndPoint(IPAddress.Loopback, 7742));
+        //    var s1 = new Server(new IPEndPoint(IPAddress.Loopback, 7741));
+        //    var s2 = new Server(new IPEndPoint(IPAddress.Loopback, 7742));
 
-            var transport = new MemoryTransport();
+        //    var transport = new MemoryTransport();
 
-            s1.Initialize(new MemoryLog(), transport, false, s2.ID);
-            s2.Initialize(new MemoryLog(), transport, false, s1.ID);
+        //    s1.Initialize(new MemoryLog(), transport, false, s2.ID);
+        //    s2.Initialize(new MemoryLog(), transport, false, s1.ID);
 
-            while (true)
-            {
-                s1.Advance();
-                s2.Advance();
-                System.Threading.Thread.Sleep(0);
+        //    while (true)
+        //    {
+        //        s1.Advance();
+        //        s2.Advance();
+        //        System.Threading.Thread.Sleep(0);
 
-                var leader = s1.CurrentState is LeaderState ? s1 : s2;
+        //        var leader = s1.CurrentState is LeaderState ? s1 : s2;
 
-                if (leader.CurrentState is LeaderState && (leader.Tick % 1000) == 0)
-                {
-                    //Console.WriteLine("create");
-                    leader.PersistedStore.Create(leader, new byte[] { (byte)leader.ID.GetHashCode() });
-                    System.Threading.Thread.Sleep(5);
-                }
-                System.Threading.Thread.Sleep(1);
+        //        if (leader.CurrentState is LeaderState && (leader.Tick % 1000) == 0)
+        //        {
+        //            //Console.WriteLine("create");
+        //            leader.PersistedStore.Create(leader, new byte[] { (byte)leader.ID.GetHashCode() });
+        //            System.Threading.Thread.Sleep(5);
+        //        }
+        //        System.Threading.Thread.Sleep(1);
 
-                //if (Console.KeyAvailable)
-                //{
-                //    var key = Console.ReadKey().KeyChar;
-                //    switch (key)
-                //    {
-                //        case 'a':
-                //    }
-                //}
-            }
+        //        //if (Console.KeyAvailable)
+        //        //{
+        //        //    var key = Console.ReadKey().KeyChar;
+        //        //    switch (key)
+        //        //    {
+        //        //        case 'a':
+        //        //    }
+        //        //}
+        //    }
 
 
-            Console.Read();
-        }
+        //    Console.Read();
+        //}
 
         //static void TestConsole()
         //{
