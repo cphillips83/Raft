a0461b8ffe06b105c55ed25d608d442148aba524
Chris Phillips <chris.phillips@mindmatrix.net>
2015-07-10 14:42:21 -0400

    temp commit

---
+++ src/Raft/States/LeaveState.cs
@@ -0,0 +1,44 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Raft.Messages;
+
+//namespace Raft.States
+//{
+//    public class LeaveState : FollowerState
+//    {
+//        public LeaveState(Server server)
+//            : base(server)
+//        {
+//            _bootstrap = bootstrap;
+//        }
+
+//        public override void Enter()
+//        {
+//            base.Enter();
+//            HeartbeatTimeout();
+//        }
+
+//        protected override void HeartbeatTimeout()
+//        {
+//            _bootstrap.SendAddServerRequest();
+//        }
+
+//        protected override bool VoteReply(Client client, VoteReply reply)
+//        {
+//            return true;
+//        }
+
+//        protected override bool AddServerRequest(Client client, AddServerRequest request)
+//        {
+//            return true;
+//        }
+
+//        protected override bool AppendEntriesReply(Client client, AppendEntriesReply reply)
+//        {
+//            return true;
+//        }
+//    }
+//}
