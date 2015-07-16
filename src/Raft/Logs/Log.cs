2e72c4a7cd03c7c2e8c586a332b3a502fd90e2b9
Chris Phillips <chris.phillips@mindmatrix.net>
2015-07-16 17:44:23 -0400

    removed lidgren from test, increase rpc to 75 and election to 150

--- src/Raft/Logs/Log.cs
+++ src/Raft/Logs/Log.cs
@@ -11,8 +11,8 @@ namespace Raft.Logs
     public abstract class Log : IDisposable
     {
 
-        public int RPC_TIMEOUT = 50;
-        public int ELECTION_TIMEOUT = 100;
+        public int RPC_TIMEOUT = 75;
+        public int ELECTION_TIMEOUT = 150;
 
         //can not change once in production
         public const int SUPER_BLOCK_SIZE = 1024;
